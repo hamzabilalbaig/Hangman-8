c801032801a77b40a03744ae1532e2d777318dbf
AntonyDavidTroy <antonydavidtroy@googlemail.com>
2021-09-26 16:37:20 +0000

    Add list of words

--- run.py
+++ run.py
@@ -1,3 +1,58 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#include <iostream>
+
+def main();
+    {
+        std::cout << "+---------------------------------+" << std::endl;
+        std::cout << "|                                 |" << std::endl;
+        std::cout << "|             HANGMAN             |" << std::endl;
+        std::cout << "|           +---------+           |" << std::endl;
+        std::cout << "+---------------------------------+" << std::endl;
+        std::cout << "|                |                |" << std::endl;
+        std::cout << "|                |                |" << std::endl;
+        std::cout << "|                O                |" << std::endl;
+        std::cout << "|               /|\               |" << std::endl;
+        std::cout << "|               / \               |" << std::endl;
+        std::cout << "|          +------------+         |" << std::endl;
+        std::cout << "|          |            |         |" << std::endl;
+        std::cout << "+---------------------------------|" << std::endl;
+        std::cout << "|        Available letters        |" << std::endl;
+        std::cout << "+---------------------------------+" << std::endl;
+        std::cout << "|                                 |" << std::endl;
+        std::cout << "|    A B C D E F G H I J K L M    |" << std::endl;
+        std::cout << "|    N O P Q R S T U V W X Y Z    |" << std::endl;
+        std::cout << "|                                 |" << std::endl;
+        std::cout << "+---------------------------------|" << std::endl;
+        std::cout << "|         Guess The Word          |" << std::endl;
+        std::cout << "+---------------------------------+" << std::endl;
+        std::cout << "|  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  |" << std::endl;
+        std::cout << "+---------------------------------+" << std::endl;  
+    }
+
+
+"""
++---------------------------------+
+|                                 |
+|             HANGMAN             |
+|           +---------+           |
++---------------------------------+
+|                |                |
+|                |                |
+|                O                |
+|               /|\               |
+|               / \               |
+|          +------------+         |
+|          |            |         |
++---------------------------------|
+|        Available letters        |
++---------------------------------+
+|                                 |
+|    A B C D E F G H I J K L M    |
+|    N O P Q R S T U V W X Y Z    |
+|                                 |
++---------------------------------|
+|         Guess The Word          |
++---------------------------------+
+|  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  |
++---------------------------------+
+>
+"""
