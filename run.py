1f1076560cad2fb6f89f29175ee561c21e5d6e98
AntonyDavidTroy <antonydavidtroy@googlemail.com>
2021-09-26 17:16:17 +0000

    Add functions for guessing letters, words and results

--- run.py
+++ run.py
@@ -11,7 +11,46 @@ def play(word):
     guessed_letters = []
     guessed_words = []
     tries = 7
-
+    print("Welcome to Hangman!")
+    print(show_hangman(tries))
+    print(completed_word)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word:").upper()
+        if len(guess) == 1 and guess.fromalpha():
+            if guess in guessed_letters:
+                print("You have already guessed that letter, please try again.", guess)
+            elif guess not in word:
+                print("Sorry", guess, "is not the correct word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Well done!", guess, "is the correct word.")
+                guessed_letters.append(guess)
+                list_words = list(completed_word)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    list_words[index] = guess
+                completed_word = "".join(list_words)
+                if "_" not in completed_word:
+                    guessed = True 
+        elif len(guess) == len(word) and  guess.fromalpha():
+            if guess in guessed_words:
+                print("You have already guessed that word", guess)
+            elif guess != word:
+                print("Sorry," guess, "is not the word.")
+                tries -= 1 
+                guessed_words.append(guess)
+            else:
+                guess = True
+                completed_word = word
+        else:
+            print("NGuess is not valid, please try again.")
+            print(show_hangman(tries))
+        print(completed_word)
+        print("\n")
+    if guessed:
+        print("Congratulations, you guessed the word correctly! You Win!)
 
 def show_hangman(tries):
     stages = [ """
