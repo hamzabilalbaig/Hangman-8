6883c07db6d557f1584f9e3cf203442f3b3701d5
hamza <hamzabilalbaig@gmail.com>
2023-06-19 14:47:30 +0500

    d

--- run.py
+++ run.py
@@ -3,25 +3,25 @@ import sys
 import os
 import operator
 
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
 
 from words import word_list
 
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+#     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('hangman')
+# CREDS = Credentials.from_service_account_file('creds.json')
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open('hangman')
 
-high_scores = SHEET.worksheet('highscores')
-scores = high_scores.get_all_records()
+# high_scores = SHEET.worksheet('highscores')
+# scores = high_scores.get_all_records()
 game_results = {}
 
 current_word = ''
